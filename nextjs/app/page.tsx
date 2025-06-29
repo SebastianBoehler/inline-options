a05a4424788739872cae683464f8ab4ba9d9371f
Sebastian Boehler <basti.boehler@hotmail.de>
2025-06-29 11:02:03 +0200

    chore: assets filter

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,14 +1,26 @@
 "use client";
+import { fetchAssets } from "./hooks/sg";
 import ProductTable from "./ProductTable";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Asset } from "./hooks/types";
 
 export default function Home() {
+  const [assets, setAssets] = useState<Asset[]>([]);
   const [limit, setLimit] = useState(50);
   const [offset, setOffset] = useState(0);
   const currentDate = useRef(new Date().toISOString().split('T')[0]);
   const [calcDateFrom, setCalcDateFrom] = useState(currentDate.current);
   const [calcDateTo, setCalcDateTo] = useState("2025-07-19");
-  const [assetId, setAssetId] = useState(undefined); // "-4"
+  const [assetId, setAssetId] = useState<string | undefined>(undefined); // "-4"
+
+  useEffect(() => {
+    async function getAssets() {
+      const assets = await fetchAssets();
+      console.log(assets);
+      setAssets(assets);
+    }
+    getAssets();
+  }, []);
 
   return (
     <main className="p-6 mx-auto">
@@ -57,6 +69,22 @@ export default function Home() {
             onChange={e => setCalcDateTo(e.target.value)}
             className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 w-44"
           />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="assetId" className="text-xs font-medium text-gray-700 mb-1">Asset ID</label>
+          <select
+            id="assetId"
+            value={assetId}
+            onChange={e => setAssetId(e.target.value.toString())}
+            className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 w-28"
+          >
+            <option value="">Select an asset</option>
+            {assets.map(asset => (
+              <option key={asset.Id} value={asset.Id}>
+                {asset.Name}
+              </option>
+            ))}
+          </select>
         </div>
       </form>
       <ProductTable
